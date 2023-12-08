1d325515fb6efd7f9227454e2a2a4637814d69cf
Eve Richardson <66529930+erichardson97@users.noreply.github.com>
2023-12-08 11:30:05 -0800

    Update normalize_Abtitre.R
    add import statement for dplyr

--- normalization_testdata/normalize_Abtitre.R
+++ normalization_testdata/normalize_Abtitre.R
@@ -1,3 +1,4 @@
+library('dplyr')
 count_data_long <- read_tsv("../raw_prediction/2022BD_plasma_ab_titer.tsv")
 df_subject_specimen <- read_tsv('../raw_prediction/2022BD_specimen.tsv')
 median_vals <- count_data_long %>% left_join(df_subject_specimen[c('specimen_id','subject_id')])  %>% mutate(isotype_antigen = paste0(isotype,"_", antigen)) %>% group_by(subject_id, isotype_antigen) %>% summarise(MFI_normalised = median(MFI_normalised)) 
@@ -6,5 +7,4 @@ median_flat <- median_vals  %>%
   pivot_wider(names_from = "isotype_antigen", values_from = MFI_normalised) %>%
   column_to_rownames("subject_id")%>%
   t() 
-
 write.table(median_flat, "IgG_titres_test.tsv",sep='\t')
