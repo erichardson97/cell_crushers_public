8c2d72b6c4339f0e1369beea04221eeac6d2906c
Eve Richardson <66529930+erichardson97@users.noreply.github.com>
2024-01-10 10:25:56 -0800

    Update modelling_genes.py

--- modelling/modelling_genes.py
+++ modelling/modelling_genes.py
@@ -81,7 +81,7 @@ def repeat_cv(data, candidate_features, args_for_cv, output_path, cv_type = 'Reg
   return total
   
 
-def load_data(path = '/content/drive/MyDrive/CMIPB_Files/IntegratedData.tsv', target = 'Day14_IgG_Titre', transform = True, keep = True):
+def load_data(path = '/content/drive/MyDrive/CMIPB_Files/IntegratedData.tsv', target = 'Day14_IgG_Titre', transform = True, keep = False):
   data = pd.read_csv(path, sep = '\t', index_col = 0)
   if ('Target' in data) & (keep == False):
     del data['Target']
