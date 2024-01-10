23cc16695107a0dbd8c68aa45a60563836b2a68e
Eve Richardson <66529930+erichardson97@users.noreply.github.com>
2024-01-10 10:36:18 -0800

    Update autogluon.py

--- modelling/autogluon.py
+++ modelling/autogluon.py
@@ -14,7 +14,7 @@ from autogluon.tabular import TabularPredictor
 
 
 data_directory = '/mnt/bioadhoc/Users/erichard/cell_crushers/data/'
-data = pd.read_csv(os.path.join(data_directory, 'IntegratedData_Normalized.tsv'), sep = '\t', index_col = 0)
+data = pd.read_csv(os.path.join(data_directory, 'correlation_filtered/CorrelationFiltered0.9.tsv'), sep = '\t', index_col = 0)
 target = 'Day14_IgG_Titre'
 data = data.rename(columns = {target: 'Target'})
 data = data[(data['Target'].notna())&(data['Titre_IgG_PT'].notna())]
@@ -22,19 +22,21 @@ data['Target'] = data['Target'].map(float).map(np.log2)
 data['Titre_IgG_PT'] = data['Titre_IgG_PT'].map(float).map(np.log2)
 
 github_directory = '/content/drive/MyDrive/Github'
+features = pd.read_pickle(os.path.join(data_directory,'AllFeatures.p'))
+candidate_features = [p for p in data.columns if 'GEX' in p] + features['cell_freq'] + features['cytokine'] + features['demographic']
 
-features_sans_target = pd.read_csv(os.path.join(data_directory, 'features_sans_target.txt'))['feature name'].values
-filtered_genes = pd.read_csv(os.path.join(data_directory, 'FilteredGenes.txt'))['feature name'].unique()
-demographic = ['age', 'biological_sex', 'infancy_vac', 'Titre_IgG_PT']
-cell_freqs = ['Cellfrequency_TcmCD4', 'Cellfrequency_TemCD4', 'Cellfrequency_Bcells','Cellfrequency_ASCs (Plasmablasts)']
-genes = [p for p in features_sans_target if 'GEX' in p]
-cytokines = [p for p in features_sans_target if 'Cytokine' in p]
-cytokines.remove('Cytokine_IFNG')
-cytokines.remove('Cytokine_TNF')
-candidate_features = list(filtered_genes) + cytokines + demographic + cell_freqs
+# features_sans_target = pd.read_csv(os.path.join(data_directory, 'features_sans_target.txt'))['feature name'].values
+# filtered_genes = pd.read_csv(os.path.join(data_directory, 'FilteredGenes.txt'))['feature name'].unique()
+# demographic = ['age', 'biological_sex', 'infancy_vac', 'Titre_IgG_PT']
+# cell_freqs = ['Cellfrequency_TcmCD4', 'Cellfrequency_TemCD4', 'Cellfrequency_Bcells','Cellfrequency_ASCs (Plasmablasts)']
+# genes = [p for p in features_sans_target if 'GEX' in p]
+# cytokines = [p for p in features_sans_target if 'Cytokine' in p]
+# cytokines.remove('Cytokine_IFNG')
+# cytokines.remove('Cytokine_TNF')
+# candidate_features = list(filtered_genes) + cytokines + demographic + cell_freqs
 
 
-output_path = '/mnt/bioadhoc/Users/erichard/CMIPB_Autogluon'
+output_path = '/mnt/bioadhoc/Users/erichard/CMIPB_Autogluon_Uncorrelated'
 cv_results = []
 for repeat in range(10):
   splits = pd.read_pickle(os.path.join(data_directory, 'cv_folds', f'Repeat{repeat}_CV_Idx.p'))
