e3397562fc45dfe54eacc5d65b80407b4a94a795
Eve Richardson <66529930+erichardson97@users.noreply.github.com>
2024-01-12 16:13:27 -0800

    Update model.py

--- modelling/model.py
+++ modelling/model.py
@@ -76,6 +76,7 @@ def run_model(model_name: str, data_dir: str, data_params: dict, model_params: d
           cv_args['transformation_args']['features_to_change'] = feature_list
         if type(transformation_args['features_to_change']) != list:
           cv_args['transformation_args']['features_to_change'] = eval(transformation_args['features_to_change'])
+        assert len(transformation_args['features_to_change']) <= len(feature_list)
         if 'reducer' in transformation_args:
           cv_args['transformation_args']['reducer'] = reducer
         cv_args['transformation'] = transformation_func
